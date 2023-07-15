f277512b2748e3a63dae83239dd30591e4b31787
Rinat273 <bairam0487@gmail.com>
2023-07-16 02:46:02 +0400

    Add Rakefile, update Gemfile, add Product model

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 
 get '/' do
 	erb :index		
