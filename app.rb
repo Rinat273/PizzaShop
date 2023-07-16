2b9625c558cd054baed47c8f9ec25be244e75b17
Rinat273 <bairam0487@gmail.com>
2023-07-17 02:58:39 +0400

    Update migration

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'sinatra/activerecord'
 
 set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 
+class Product < ActiveRecord::Base
+end
+
 get '/' do
 	erb :index		
 end
--- db/migrate/20230716131505_create_products.rb
+++ db/migrate/20230716131505_create_products.rb
@@ -1,17 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-     create_table :products do |t|
+    create_table :products do |t|
         t.string :title
-        t.text   :description
+        t.text :description
         t.decimal :price
         t.decimal :size
         t.boolean :is_spicy
         t.boolean :is_veg
         t.boolean :is_best_offer
         t.string :path_to_image
-      
-      t.timestamps
-    end
 
+        t.timestamps
+    end
   end
 end
